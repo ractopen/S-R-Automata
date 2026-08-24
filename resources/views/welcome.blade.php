2552fd8b6ca4b6b8fadc96c2172b4eca350b9e0c
Naes-lame <seancalixton369@gmail.com>
2026-08-24 21:30:10 +0800

    Update system name to RYSE, generate and add favicon, and rehaul email OTP templates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'RYSE') }}</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 </head>
 <style>
     body{
